2463e5a8e700e5a2cfa64c56ab8b79d66054f4ae
nuraiym <jmnuraika@gmail.com>
2024-12-23 18:23:06 +0600

    celine flowers

---
+++ js.js
@@ -0,0 +1,13 @@
+let burger = document.querySelector('.links-right')
+
+let toggleBurger = ()=>{
+    burger.classList.toggle('active')
+    
+}
+
+let menu = document.querySelector('.links-left')
+
+let toggleMenu = ()=>{
+    menu.classList.toggle('active')
+    
+}
